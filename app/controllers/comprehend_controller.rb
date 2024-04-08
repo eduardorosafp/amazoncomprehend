261fb359a569ad3d8b20b92e6d2707adccff452f
eduardorosafp <eduardorosafp@gmail.com>
2024-04-08 15:11:47 -0300

    amazoncomprehend

--- app/controllers/comprehend_controller.rb
+++ app/controllers/comprehend_controller.rb
@@ -1,17 +1,27 @@
+require 'aws-sdk-comprehend'
+
 class ComprehendController < ApplicationController
-  require 'aws-sdk-comprehend'
-
   def analyze_text
-    text = "Eduardo esta muito triste"
+    # Configuração do cliente Comprehend
+    comprehend = Aws::Comprehend::Client.new(region: 'us-east-2')
 
-    response = COMPREHEND_CLIENT.detect_sentiment({
-      text: text,
-      language_code: "pt"
-    })
+    # Parâmetros para o job de detecção de tópicos
+    job_params = {
+      input_data_config: {
+        s3_uri: 's3://eduardoteste2/Documento sem título.txt', # URI do arquivo de texto a ser analisado
+        input_format: 'ONE_DOC_PER_LINE' # Formato do arquivo de entrada (um documento por linha)
+      },
+      output_data_config: {
+        s3_uri: 's3://eduardoteste2/output/891377055217-TOPICS-8ff77d846a46b382e1c6668a0393b242/output/output.tar.gz' # URI do local onde os resultados serão salvos
+      },
+      data_access_role_arn: 'arn:aws:iam::891377055217:role/service-role/AmazonComprehendServiceRole-eduardorosateste',
+      job_name: 'eduardoteste4' # Nome do job
+    }
 
-    puts "Sentimento: #{response.sentiment}"
-    puts "Pontuação: #{response.sentiment_score}"
+    # Iniciar o job de detecção de tópicos
+    response = comprehend.start_topics_detection_job(job_params)
 
-    render json: response.sentiment_score
+    # Saída da chamada da API
+    render json: response.to_h
   end
 end
